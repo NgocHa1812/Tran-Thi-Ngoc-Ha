0b28ef59b1ecfaa84c1c500cd21a24ddcec49c1e
NgocHa1812 <64211443+NgocHa1812@users.noreply.github.com>
2020-05-05 23:00:24 +0700

    Add files via upload

---
+++ FrmTinhTong_bT1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Chuong4_Part1bt
+{
+    partial class FrmTinhTong_bT1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnhapA = new System.Windows.Forms.TextBox();
+            this.txtnhapB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTongAB = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập a =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập b =";
+            // 
+            // txtnhapA
+            // 
+            this.txtnhapA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnhapA.Location = new System.Drawing.Point(252, 64);
+            this.txtnhapA.Name = "txtnhapA";
+            this.txtnhapA.Size = new System.Drawing.Size(172, 30);
+            this.txtnhapA.TabIndex = 2;
+            this.txtnhapA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnhapA_KeyPress);
+            // 
+            // txtnhapB
+            // 
+            this.txtnhapB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnhapB.Location = new System.Drawing.Point(252, 127);
+            this.txtnhapB.Name = "txtnhapB";
+            this.txtnhapB.Size = new System.Drawing.Size(172, 30);
+            this.txtnhapB.TabIndex = 3;
+            this.txtnhapB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnhapB_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(247, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Có tổng là";
+            // 
+            // btnTongAB
+            // 
+            this.btnTongAB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTongAB.Location = new System.Drawing.Point(79, 257);
+            this.btnTongAB.Name = "btnTongAB";
+            this.btnTongAB.Size = new System.Drawing.Size(87, 34);
+            this.btnTongAB.TabIndex = 5;
+            this.btnTongAB.Text = "Tổng";
+            this.btnTongAB.UseVisualStyleBackColor = true;
+            this.btnTongAB.Click += new System.EventHandler(this.btnTongAB_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLamLai.Location = new System.Drawing.Point(284, 257);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(84, 34);
+            this.btnLamLai.TabIndex = 6;
+            this.btnLamLai.Text = "Làm lại";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(493, 257);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(79, 34);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // FrmTinhTong_bT1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnTongAB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnhapB);
+            this.Controls.Add(this.txtnhapA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmTinhTong_bT1";
+            this.Text = "FrmTinhTong_bT1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnhapA;
+        private System.Windows.Forms.TextBox txtnhapB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTongAB;
+        private System.Windows.Forms.Button btnLamLai;
+        private System.Windows.Forms.Button btnThoat;
+        
+    }
+}
---
+++ FrmTinhTong_bT1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuong4_Part1bt
+{
+    public partial class FrmTinhTong_bT1 : Form
+    {
+        public FrmTinhTong_bT1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtnhapA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8) || (Convert.ToInt32(e.KeyChar) == 13))
+
+                e.Handled = false;
+
+            else e.Handled = true;
+        }
+
+        private void txtnhapB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8) || (Convert.ToInt32(e.KeyChar) == 13))
+
+                e.Handled = false;
+
+            else e.Handled = true;
+        }
+
+        private void btnTongAB_Click(object sender, EventArgs e)
+        {
+            if (txtnhapA.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập số a");
+                txtnhapA.Focus();
+                return;
+            }
+            if (txtnhapB.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập tên");
+                txtnhapB.Focus();
+                return;
+            }
+            double tong = 0;
+            double a = Convert.ToDouble(txtnhapA.Text);
+            double b = Convert.ToDouble(txtnhapB.Text);
+
+
+            if (a <= b)
+            {
+                for (double i = a; i <= b; i++)
+                    tong += i;
+
+            }
+            if (a > b)
+            {
+                for (double i = b; i <= a; i++)
+                    tong += i;
+            }
+            label1.Text = a.ToString();
+            label2.Text = b.ToString();
+
+            label3.Text = tong.ToString();
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            txtnhapA.Clear();
+            txtnhapB.Clear();
+            txtnhapA.Focus();
+        }
+    }
+}
